f62de33d01b783ba5504d58f09012e64b36df238
Efim-Kapliy <efim0424@mail.ru>
2023-10-10 05:11:37 +0300

    Layout using Bootstrap and React

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учёт сотрудников в компании Thermwood</h1>
+      <h2>Общее число сотрудников: </h2>
+      <h2>Премию получат: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,7 @@
+import "./search-panel.css";
+
+const SearchPanel = () => {
+  return <input type="text" className="form-control search-input" placeholder="Найти сотрудника" />;
+};
+
+export default SearchPanel;
