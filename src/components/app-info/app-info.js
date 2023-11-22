fa44dece85d5716e3dae644067f68be644b997f9
Efim-Kapliy <efim0424@mail.ru>
2023-11-22 18:48:33 +0300

    feat: add a display of the total number and bonus employees

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({ employees, increased }) => {
   return (
     <div className="app-info">
       <h1>Учёт сотрудников в компании Thermwood</h1>
-      <h2>Общее число сотрудников: </h2>
-      <h2>Премию получат: </h2>
+      <h2>Общее число сотрудников: {employees}</h2>
+      <h2>Премию получат: {increased}</h2>
     </div>
   );
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -77,9 +77,12 @@ class App extends Component {
   };
 
   render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
+
     return (
       <div className="app">
-        <AppInfo />
+        <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
           <SearchPanel />
