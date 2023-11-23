8b8ea4b1441254e6ed79aef7373494f57229c773
Efim-Kapliy <efim0424@mail.ru>
2023-11-24 01:36:55 +0300

    feat: add filter by name

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,7 @@ class App extends Component {
         { name: "Efim K.", salary: 3000, increase: false, rise: false, id: 2 },
         { name: "Kirill S.", salary: 5000, increase: true, rise: false, id: 3 },
       ],
+      term: "",
     };
     this.maxId = 4;
   }
@@ -65,20 +66,35 @@ class App extends Component {
     }));
   };
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) return items;
+
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
+
   render() {
-    const employees = this.state.data.length;
-    const increased = this.state.data.filter((item) => item.increase).length;
+    const { data, term } = this.state;
+
+    const employees = data.length;
+    const increased = data.filter((item) => item.increase).length;
+    const visibleData = this.searchEmp(data, term);
 
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
 
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
           <AppFilter />
         </div>
 
-        <EmployeesList data={this.state.data} onDelete={this.deleteItem} onToggleProp={this.onToggleProp} />
+        <EmployeesList data={visibleData} onDelete={this.deleteItem} onToggleProp={this.onToggleProp} />
         <EmployeesAddForm onAdd={this.addItem} />
       </div>
     );
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,7 +1,32 @@
+import { Component } from "react";
+
 import "./search-panel.css";
 
-const SearchPanel = () => {
-  return <input type="text" className="form-control search-input" placeholder="Найти сотрудника" />;
-};
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
+}
 
 export default SearchPanel;
