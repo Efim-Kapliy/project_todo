070f23f30f49738eaf3dd8c1e8a0545f6eb9bc09
Efim-Kapliy <efim0424@mail.ru>
2023-11-24 18:44:46 +0300

    feat: add salary validation in the item

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,9 +5,20 @@ import "./employees-list.css";
 const EmployeesList = ({ data, onDelete, onToggleProp, onItemSalary }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
-    const itemReplaceAttribute = (e) => e.currentTarget.getAttribute("data-replace");
+
+    const itemReplaceAttribute = (e) => {
+      e.currentTarget.getAttribute("data-replace");
+    };
+
     const itemReplaceValue = (e) => {
       if (e.currentTarget.getAttribute("data-replace") === "salary") {
+        let position = e.target.selectionStart;
+
+        e.target.value = e.target.value.replace(/\D/g, "");
+        e.target.value = e.target.value.replace(/$/g, "$");
+        if (e.target.value.length <= 1) e.target.value = e.target.value.replace(/^/g, "0");
+
+        e.target.selectionEnd = position;
         return e.target.value;
       }
       return e.target.value;
